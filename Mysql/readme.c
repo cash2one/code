f40bb1ebac2dc3f84264fb9e79461a584249d4b5
王 天耀 <yao56383702@oumatoMacBook-Pro.local>
2013-02-16 15:50:19 +0800

    readme

---
+++ Mysql/readme.c
@@ -0,0 +1,2 @@
+gcc insert.c -Wall -I/usr/include -L/usr/lib -lmysqlclient -o insert 							// linux
+gcc insert.c -Wall -I/usr/local/mysql/include -L/usr/local/mysql/lib -lmysqlclient -o insert    // mac
